2d943002bab4b93bf8f4df55477c2995f9f50b8b
MinkieYume <sign@yumieko.com>
2025-09-22 10:03:02 +0800

    kavita-test

---
+++ chiko/services/web.scm
@@ -0,0 +1,90 @@
+(define-module (chiko services web)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match)
+  #:use-module (guix gexp)
+  #:use-module (guix records)
+  #:use-module (guix packages)  
+  #:use-module (gnu packages file-systems)
+  #:use-module (gnu packages linux)  
+  #:use-module (gnu services)
+  #:use-module (gnu services admin)  
+  #:use-module (gnu services configuration)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu system shadow)
+  #:use-module (srfi srfi-13)
+  #:export ())
+
+(define %kavita-accounts
+  (list (user-account
+          (name "kavita")
+          (group "docker")
+          (system? #t)
+          (home-directory "/var/empty")
+          (shell (file-append shadow "/sbin/nologin")))))
+
+;; (define %netbird-postgresql-role
+;;   (list (postgresql-role
+;;          (name "netbird")
+;;          (create-database? #t))))
+
+(define-configuration/no-serialization kavita-configuration
+  (image
+   (string "jvmilazz0/kavita:latest")
+   "")
+  (port
+   (number 5000)
+   "")
+  (data-directory
+   (string "/var/lib/kavita")
+   "")
+  (time-zone
+   (string "Asia/Shanghai")
+   "")
+  (log-file
+   (string "/var/log/kavita.log")
+   ""))
+
+(define kavita-activation
+  (match-record-lambda <kavita-configuration>
+      (data-directory log-file)
+    #~(begin
+        (use-modules (guix build utils))
+        (let ((user (getpwnam "kavita")))
+          (unless (file-exists? #$data-directory)
+            (mkdir-p #$data-directory)
+            (chown #$data-directory (passwd:uid user) (passwd:gid user)))
+          (unless (file-exists? #$log-path)
+            (mkdir-p #$log-path)
+            (chown #$log-path (passwd:uid user) (passwd:gid user)))))))
+
+(define kavita-oci-container
+  (match-record-lambda <kavita-configuration>
+      (image log-file port data-directory time-zone)
+    (list (oci-container-configuration
+            (user "kavita")
+            (group "docker")
+            (image image)
+            (provision "kavita")
+            (log-file log-file)
+            (respawn? #t)
+            (ports `((,port . ,port)))
+            (environment (list '("TZ" . time-zone)))
+            (volumes
+             `((,(string-append data-directory "/manga") . "/manga")
+               (,(string-append data-directory "/comics") . "/comics")
+               (,(string-append data-directory "/books") . "/books")
+               (,(string-append data-directory "/config") . "/kavita/config")))))))
+
+(define kavita-service-type
+  (service-type
+    (name 'kavita)
+    (extensions
+     (list (service-extension account-service-type
+                              (const %kavita-accounts))
+           (service-extension log-rotation-service-type
+                              (compose list kavita-configuration-log-file))
+           (service-extension activation-service-type
+                              kavita-activation)
+           (service-extension oci-container-service-type
+                              kavita-oci-container)))
+    (description "运行Netbird Management服务。")))
